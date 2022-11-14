35d3164cfb7b1a1324f6dea483c7a0e545c4b1bd
catacola199 <fufideira17@gmail.com>
2022-11-14 15:34:57 +0700

    12

--- application/views/dashboard/kalibrasi.php
+++ application/views/dashboard/kalibrasi.php
@@ -243,7 +243,7 @@
                             </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-danger" data-dismiss="modal"> <i class="fa fa-window-close"></i> Batal</button>
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal"> <i class="fa fa-window-close"></i> Batal</button>
                         <button type="submit" class="btn btn-success"><i class="fa fa-save"></i> Update</button>
                     </div>
                     </form>
