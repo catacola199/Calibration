9a68ab1a90fd7e2bcd53ef754d52a67b99fe80f4
catacola199 <fufideira17@gmail.com>
2022-11-14 16:24:52 +0700

    12

--- application/views/component/_header.php
+++ application/views/component/_header.php
@@ -62,11 +62,6 @@
                     <div class="dropdown-menu dropdown-menu-right user-dd animated flipInY">
                         <a class="dropdown-item" href="<?= base_url('profiles')?>"><i data-feather="user" class="svg-icon mr-2 ml-1"></i>
                             My Profile</a> 
-                        <a class="dropdown-item" href="javascript:void(0)"><i data-feather="mail" class="svg-icon mr-2 ml-1"></i>
-                            Inbox</a>
-                        <a class="dropdown-item" href="javascript:void(0)"><i data-feather="settings" class="svg-icon mr-2 ml-1"></i>
-                            Account Setting</a>
-                        
                         <hr class="dropdown-divider">
                         <a class="dropdown-item" href="<?= base_url('login/logout') ?>"><i data-feather="power" class="svg-icon mr-2 ml-1"></i>
                             Logout</a>
